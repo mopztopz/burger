0dc6cd0461aca791804ad063e6e47ee80d7e0d6f
Mopztopz <stephenbrown83@gmail.com>
2019-02-14 02:11:52 -0600

    just trying to get it to work

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection;
 
-if(process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-  });
-}
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "burgers_db"
+});
+
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
@@ -26,6 +22,3 @@ connection.connect(function(err) {
 
 // Export connection for our ORM to use.
 module.exports = connection;
-
-
-
